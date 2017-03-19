65d8a35310035dd580d72655a07930c3601e1cc5
ctadmin <zubairmajestic@gmail.com>
2017-03-19 09:31:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ctadmin/myapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.ctadmin.myapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TextView mytextView = (TextView) findViewById(R.id.textView);
+                mytextView.setText("hi..i changed the text...");
+            }
+        });
+    }
+}
